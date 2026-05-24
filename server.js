8c7c283bb043a52fec501ff9539f99e9227497dc
SEJE-codes <modricalcantara610@gmail.com>
2026-05-24 14:39:28 +0200

    Updated production database

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ const app = express();
 // ========================
 // MIDDLEWARE
 // ========================
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://best-digital-platform-qr2j.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -19,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 // ========================
 app.use(
   "/uploads",
-  express.static(path.join(__dirname, "uploads"))
+  express.static("uploads")
 );
 
 // ========================
