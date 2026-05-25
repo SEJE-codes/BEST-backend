a1d738c38bb4a20fcfad3c3b65a90d089dc2d8a3
SEJE-codes <modricalcantara610@gmail.com>
2026-05-25 11:11:52 +0200

    Fix CORS issue

--- server.js
+++ server.js
@@ -18,19 +18,15 @@ const allowedOrigins = [
   "http://localhost:5173"
 ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+import cors from "cors";
 
+app.use(cors({
+  origin: [
+    "https://best-digital-platform.vercel.app",
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
 // ======================================
 // BODY PARSER
 // ======================================
